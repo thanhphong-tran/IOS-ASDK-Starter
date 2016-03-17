4291c9a59493573b160b35234829868b9b41e847
Luke Parham <lappp9@gmail.com>
2016-03-17 18:06:48 -0500

    pods

--- RainForestFinal/AnimalTableNodeController.m
+++ RainForestFinal/AnimalTableNodeController.m
@@ -8,7 +8,7 @@
 static NSString *kCellReuseIdentifier = @"CellReuseIdentifier";
 
 @interface AnimalTableNodeController ()<UITableViewDataSource, UITableViewDelegate>
-@property (nonatomic, strong) astable *tableView;
+@property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSArray *animals;
 @end
 
