fc73efe80d284cacdaa765e0957c8aaca0da16f0
Luke Parham <lappp9@gmail.com>
2016-03-18 15:30:31 -0500

    made background black

--- RainForestFinal/AnimalTableNodeController.m
+++ RainForestFinal/AnimalTableNodeController.m
@@ -33,7 +33,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+
+    self.view.backgroundColor = [UIColor blackColor];
 }
 
 #pragma mark ASTableNode DataSource
--- RainForestFinal/AppDelegate.m
+++ RainForestFinal/AppDelegate.m
@@ -10,7 +10,6 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     CGRect windowRect = [[UIScreen mainScreen] bounds];
